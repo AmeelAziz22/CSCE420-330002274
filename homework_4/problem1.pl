30c981513e886812c0ef1af2d42e0b904533b789
ameelaziz <ameelaziz@tamu.edu>
2023-11-27 16:59:17 -0600

    finished prolog

---
+++ homework_4/problem1.pl
@@ -0,0 +1,125 @@
+% problem 1 facts
+parent(bart,homer).
+parent(bart,marge).
+parent(lisa,homer).
+parent(lisa,marge).
+parent(maggie,homer).
+parent(maggie,marge).
+parent(homer,abraham).
+parent(herb,abraham).
+parent(tod,ned).
+parent(rod,ned).
+parent(marge,jackie).
+parent(patty,jackie).
+parent(selma,jackie).
+female(maggie).
+female(lisa).
+female(marge).
+female(patty).
+female(selma).
+female(jackie).
+male(bart).
+male(homer).
+male(herb).
+male(burns).
+male(smithers).
+male(tod).
+male(rod).
+male(ned).
+male(abraham).
+% problem 1 rules
+brother(X,Y) :- male(Y), parent(Y,Z), parent(X,Z), X\=Y.
+sister(X,Y) :- female(Y), parent(Y,Z), parent(X,Z), X\=Y.
+aunt(X,Y) :- female(Y), parent(X,Z), sister(Z,Y).
+uncle(X,Y) :- male(Y), parent(X,Z), brother(Z,Y).
+grandfather(X,Y) :- male(Y), parent(X,Z), parent(Z,Y).
+granddaughter(X,Y) :- female(Y), parent(Y,Z), parent(Z,X).
+ancestor(X,Y) :- parent(X,Y).
+ancestor(X,Y) :- parent(X,Z), ancestor(Z,Y).
+
+%problem 2 facts
+occupation(joe,oral_surgeon).
+occupation(sam,patent_lawyer).
+occupation(bill,trial_lawyer).
+occupation(cindy,investment_banker).
+occupation(joan,civil_lawyer).
+occupation(len,plastic_surgeon).
+occupation(lance,heart_surgeon).
+occupation(frank,brain_surgeon).
+occupation(charlie,plastic_surgeon).
+occupation(lisa,oral_surgeon).
+address(joe,houston).
+address(sam,pittsburgh).
+address(bill,dallas).
+address(cindy,omaha).
+address(joan,chicago).
+address(len,college_station).
+address(lance,los_angeles).
+address(frank,dallas).
+address(charlie,houston).
+address(lisa,san_antonio).
+salary(joe,50000).
+salary(sam,150000).
+salary(bill,200000).
+salary(cindy,140000).
+salary(joan,80000).
+salary(len,70000).
+salary(lance,650000).
+salary(frank,85000).
+salary(charlie,120000).
+salary(lisa,190000).
+%problem 2 queries
+
+query2a(X) :- occupation(X,patent_lawyer); occupation(X,trial_lawyer); occupation(X,civil_lawyer).
+surgeon(X) :- occupation(X,oral_surgeon); occupation(X,brain_surgeon); occupation(X,heart_surgeon); occupation(X,plastic_surgeon).
+california(X) :- address(X,los_angeles).
+query2b(X) :-  surgeon(X), california(X).
+texas(X) :- address(X,dallas); address(X,houston); address(X,college_station); address(X,san_antonio).
+salaryhigh(X) :- salary(X,Salary), Salary > 100000.
+query2c(X) :- salaryhigh(X), texas(X), surgeon(X).
+
+%problem3 facts
+subject(algebra,math).
+subject(calculus,math).
+subject(dynamics,physics).
+subject(electromagnetism,physics).
+subject(nuclear,physics).
+subject(organic,chemistry).
+subject(inorganic,chemistry).
+degree(bill,phd,chemistry).
+degree(john,bs,math).
+degree(chuck,ms,physics).
+degree(susan,phd,math).
+retired(bill).
+
+%problem 3 rules 
+canTeach(X,Y) :- degree(X,phd,Z), subject(Y,Z).
+canTeach2(X,Y) :- canTeach(X,Y), \+ retired(X).
+canTeachMS(X,Y) :- degree(X,ms,Z), subject(Y,Z), \+ retired(X).
+canTeach3(X,Y) :- canTeach2(X,Y); canTeachMS(X,Y).
+
+%problem 4
+
+bit(0).
+bit(1).
+
+octal_code(A, B, C) :-
+    bit(A),
+    bit(B),
+    bit(C),
+    format('~w~w~w~n', [A, B, C]).
+
+%problem5
+
+
+
+color(red).
+color(green).
+color(blue).
+
+mapcolor(WA,NT,SA,Q,NSW,V,T) :- 
+    color(WA),color(NT), color(SA), color(Q), color(NSW), color(V), color(T),
+    WA \== NT,WA \== SA, NT \== SA, NT \== Q, SA \== Q, SA \== NSW, Q \== NSW, NSW \== V.
+
+
+
